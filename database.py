e17fdba5cd4c67fa011db2b09e7fbad580d8c248
NHAlvee <73606235+NHAlvee@users.noreply.github.com>
2021-11-08 22:05:22 +0600

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-def setPokemonData():  # some sample pokemons for testing
-    # pokemon=type,stage,next pokemon to evolve into
-    pokemons = dict()
-    pokemons[1] = ("Bulbasaur", "grass", 1, 2, 45, 57, 57, 16)
-    pokemons[5] = ("Charmander", "fire", 1, 5, 39, 56, 47, 16)
-    pokemons[10] = ("Squirtle", "water", 1, 8, 44, 49, 65, 16)
-    pokemons[15] = ("Pidgey", "flying", 1, 17, 40, 40, 37, 18)
-    pokemons[20] = ("Rattata", "normal", 1, 20, 30, 41, 35, 20)
-    return pokemons
