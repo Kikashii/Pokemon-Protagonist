26a2a635699c913b24c2ba21fb153916bf677447
NHAlvee <73606235+NHAlvee@users.noreply.github.com>
2021-11-08 22:04:41 +0600

    Delete button.py

--- button.py
+++
@@ -1,8 +0,0 @@
-def inPlay(x, y):
-    x0, y0, x1, y1 = 20, 20, 90, 90
-    return x < x1 and x > x0 and y > y0 and y < y1
-
-
-def inPause(x, y):
-    x0, y0, x1, y1 = 110, 20, 180, 90
-    return x < x1 and x > x0 and y > y0 and y < y1
